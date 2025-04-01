8fd61b612ba4477374c534fe7ad8ee5bf2854022
Luis Flórez <luis.florez@hijolusa.es>
2025-04-01 09:53:26 +0200

    Añadida la url de empleados centro de maquina

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 
 
 urlEmpleados = "http://192.168.1.231:7048/BC_Test/ODataV4/Company('AGRICOLA%20VILLENA%20SL')/Empleados"  # Replace with your actual endpoint
+urlEmpleadosCentro = "http://192.168.1.231:7048/BC_Test/ODataV4/Company(%27AGRICOLA%20VILLENA%20SL%27)/CM"
 usernameBC = "SQL-NAV-PRE\\ALFREDO.BLANCO"  # Replace with your Business Central username. Note the double backslash.
 passwordBC = "Rol62357"  # Replace with your Business Central password
 
@@ -95,8 +96,9 @@ def load_setup_from_json(filepath=os.path.join(os.path.dirname(__file__), "setup
     try:
         with open(rutaJson, 'r') as f:
             data = json.load(f)
-            global urlEmpleados, usernameBC, passwordBC, userDb, passwordDb, ipDb, db, query
-            urlEmpleados = data.get('urlEmpleados', urlEmpleados)            
+            global urlEmpleados, urlEmpleadosCentro, usernameBC, passwordBC, userDb, passwordDb, ipDb, db, query
+            urlEmpleados = data.get('urlEmpleados', urlEmpleados)  
+            urlEmpleadosCentroMaquina = data.get('urlEmpleados', urlEmpleadosCentro)           
             usernameBC = data.get('usernameBC', usernameBC)            
             passwordBC = data.get('passwordBC', passwordBC)            
             userDb = data.get('userDb', userDb)            
